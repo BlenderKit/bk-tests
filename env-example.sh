4ead101a99bd6e04d8d85a7b99d8564b11fcdb94
agajdosi <andreas.gajdosik@gmail.com>
2021-10-11 00:41:10 +0200

    Add env-example.sh for quick setup of environment

---
+++ env-example.sh
@@ -0,0 +1,6 @@
+# Use this file for quick setup of test environment
+# Rename to env.sh to make sure it is ignored by git
+
+export BK_USERNAME="set username of test account here"
+export BK_PASSWORD="set password of test account here"
+source venv/bin/activate
